e12a4c108c5564b538c0321fd41cb78051dd4c76
Evgeniy <evgpashko92@gmail.com>
2022-12-08 14:45:10 +0700

    DOM Element Selection & OOP Rendering

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    // альтернативный способ
+    // this.templateElement = <HTMLTemplateElement>document.getElementById("project-input")!;
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projInput = new ProjectInput();
